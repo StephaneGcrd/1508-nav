70d5cd24eed51481cd2c1752838ddaa3a750a049
Stephane Guichard <stephaneguichard@pc-79.home>
2019-08-13 11:31:36 +0200

    Updates

---
+++ precache-manifest.5e22117aa455e5e3b4c545a06fc50570.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce3a62844d36316c6ef4db7b1b60335",
+    "url": "/1508-nav/index.html"
+  },
+  {
+    "revision": "c3f74844b25e2dca67b6",
+    "url": "/1508-nav/static/css/main.b52a6367.chunk.css"
+  },
+  {
+    "revision": "e30ee2c56ce87508a3fb",
+    "url": "/1508-nav/static/js/2.36295a59.chunk.js"
+  },
+  {
+    "revision": "c3f74844b25e2dca67b6",
+    "url": "/1508-nav/static/js/main.42082b2d.chunk.js"
+  },
+  {
+    "revision": "199cc5d0e24ea23b90ce",
+    "url": "/1508-nav/static/js/runtime~main.22d9a6e8.js"
+  }
+]);
